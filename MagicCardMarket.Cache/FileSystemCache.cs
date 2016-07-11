7d865240f235bfa6c2973e989c7422a4e6673d91
SinaC <heymbeeck.joel@gmail.com>
2016-07-11 22:43:15 +0200

    Commit problem

---
+++ MagicCardMarket.Cache/FileSystemCache.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace MagicCardMarket.Cache
+{
+    public class FileSystemCache : ICache
+    {
+        public string RootPath { get; }
+
+        public FileSystemCache(string rootPath)
+        {
+            RootPath = rootPath;
+        }
+
+        public bool Contains(string category, int id)
+        {
+            string filename = Path.Combine(RootPath, category, id.ToString()) + ".xml";
+            return File.Exists(filename);
+        }
+
+        public void Set(string category, int id, string xml)
+        {
+            string path = Path.Combine(RootPath, category);
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            string filename = Path.Combine(path, id.ToString())+".xml";
+            File.WriteAllText(filename, xml);
+        }
+
+        public string Get(string category, int id)
+        {
+            string filename = Path.Combine(RootPath, category, id.ToString()) + ".xml";
+            if (File.Exists(filename))
+                return File.ReadAllText(filename);
+            return null;
+        }
+    }
+}
---
+++ MagicCardMarket.Cache/ICache.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicCardMarket.Cache
+{
+    public interface ICache
+    {
+        bool Contains(string category, int id);
+        void Set(string category, int id, string xml);
+        string Get(string category, int id);
+    }
+}
